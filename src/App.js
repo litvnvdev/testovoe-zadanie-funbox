7bccebd649543df4f2ed164e5a0f74b2e753f1fa
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-01-19 01:46:41 +0300

    work with array card data and also styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
+import { useMemo } from 'react';
 import './App.css';
 import Card from './Components/Card';
+
 function App() {
+  const cardData = useMemo(
+    () => [
+      {
+        id: 1,
+        title: 'с фуа-гра',
+        weight: '0,5',
+        portion: '10',
+        mouse: 'мышь ',
+        description: 'Чего сидишь? Порадуй, котэ',
+      },
+      {
+        id: 2,
+        title: 'с рыбой',
+        weight: '2',
+        portion: '40',
+        mouse: ' 2 мыши',
+        description: 'Головы шучьи с чесноком да свежайшая сёмгушка.',
+      },
+      {
+        id: 3,
+        title: 'с курой',
+        weight: '5',
+        portion: '100',
+        mouse: '5 мышей ',
+        description: 'Печалька, с курой закончился.',
+      },
+    ],
+    [],
+  );
   return (
     <div className="App">
       <header className="App-header">
         <p>Ты сегодня покормил кота?</p>
       </header>
       <div className="flex-container">
-        <Card />
-        <Card />
-        <Card />
+        {cardData.map(({ title, weight, id, portion, mouse, description }) => (
+          <div key={id}>
+            <Card title={title} weight={weight} id={id} portion={portion} mouse={mouse} />
+            <p className="call-to-action-text">{description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,26 +2,7 @@ import React from 'react';
 import imgCat from '../img/cat.png';
 import '../css/Card.css';
 
-const Card = () => {
-  const cardData = {
-    cardOne: {
-      title: 'с фуа-гра',
-      weight: '0,5',
-      portion: '10',
-    },
-    cardTwo: {
-      title: 'с рыбой',
-      weight: '2',
-      portion: '40',
-      mouse: '2',
-    },
-    cardThree: {
-      title: 'с курой',
-      weight: '0,5',
-      portion: '100',
-      mouse: '5',
-    },
-  };
+const Card = ({ title, weight, portion, mouse }) => {
   return (
     <div className="wrapper">
       <div className="content">
@@ -34,19 +15,19 @@ const Card = () => {
                 </div>
                 <div className="card__title">
                   <h1>
-                    Нямушка <p className="flavour">{cardData.cardOne.title}</p>
+                    Нямушка <p className="flavour">{title}</p>
                   </h1>
                 </div>
                 <div className="card__description">
                   <p>
-                    <b>{cardData.cardOne.portion} </b>порций <br /> мышь в подарок
+                    <b>{portion} </b>порций <br /> {mouse} в подарок
                   </p>
                 </div>
               </div>
               <span className="dot">
                 {' '}
                 <p className="dot-text">
-                  {cardData.cardOne.weight} <br />
+                  {weight} <br />
                   <span>кг</span>
                 </p>
               </span>
@@ -57,12 +38,11 @@ const Card = () => {
           </div>
         </div>
       </div>
-      <p className="call-to-action-text">
-        Чего сидишь? Порадуй, котэ{' '}
+      {/* <p className="call-to-action-text">
         <a href="#" target="_blank" rel="noreferrer">
           купи.
         </a>
-      </p>
+      </p> */}
     </div>
   );
 };
