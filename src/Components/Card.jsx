1bd42536c7c1fa4926d9eb23e96cfa0fa7dce087
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-01-18 22:51:29 +0300

    work with css, try add some dynamic data

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,6 +3,25 @@ import imgCat from '../img/cat.png';
 import '../css/Card.css';
 
 const Card = () => {
+  const cardData = {
+    cardOne: {
+      title: 'с фуа-гра',
+      weight: '0,5',
+      portion: '10',
+    },
+    cardTwo: {
+      title: 'с рыбой',
+      weight: '2',
+      portion: '40',
+      mouse: '2',
+    },
+    cardThree: {
+      title: 'с курой',
+      weight: '0,5',
+      portion: '100',
+      mouse: '5',
+    },
+  };
   return (
     <div className="wrapper">
       <div className="content">
@@ -15,28 +34,35 @@ const Card = () => {
                 </div>
                 <div className="card__title">
                   <h1>
-                    Нямушка <p className="flavour">с фуа-гра</p>
+                    Нямушка <p className="flavour">{cardData.cardOne.title}</p>
                   </h1>
                 </div>
                 <div className="card__description">
                   <p>
-                    10 порций <br /> мышь в подарок
+                    <b>{cardData.cardOne.portion} </b>порций <br /> мышь в подарок
                   </p>
                 </div>
               </div>
+              <span className="dot">
+                {' '}
+                <p className="dot-text">
+                  {cardData.cardOne.weight} <br />
+                  <span>кг</span>
+                </p>
+              </span>
               <div className="card__image">
                 <img src={imgCat} alt="cat" />
               </div>
             </div>
-            <p>
-              Чего сидишь? Порадуй, котэ{' '}
-              <a href="#" target="_blank" rel="noreferrer">
-                купи.
-              </a>
-            </p>
           </div>
         </div>
       </div>
+      <p className="call-to-action-text">
+        Чего сидишь? Порадуй, котэ{' '}
+        <a href="#" target="_blank" rel="noreferrer">
+          купи.
+        </a>
+      </p>
     </div>
   );
 };
